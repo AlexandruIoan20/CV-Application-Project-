5e0dac9cf3addaaedf04229c85ecd7d6b5049a35
AlexandruIoan20 <ialexmoraru@yahoo.ro>
2023-02-09 07:54:09 +0200

    Added buttons

--- cv-app/src/App.jsx
+++ cv-app/src/App.jsx
@@ -11,36 +11,46 @@ import IntroSection from './composed_components/introduction.jsx';
 import ImageGenerator from './components/imageGenerator.jsx';
 
 
-export default class App extends Component { 
-  constructor (props) { 
+export default class App extends Component {
+  constructor(props) {
     super(props);
 
-    this.state  = { 
-      showAllButtons:  true,
+    this.state = {
+      showAllButtons: true,
     }
   }
 
-  hideButtons = () =>  { 
+  hideButtons = () => {
     const showAllButtons = !this.state.showAllButtons;
 
-    this.setState ( {...this.state, showAllButtons});
+    this.setState({ ...this.state, showAllButtons });
   }
 
-  render () {
-    const { showAllButtons } = this.state;  
-    return ( 
+  render() {
+    const { showAllButtons } = this.state;
+    return (
       <section className='container'>
-        <IntroSection showAllButtons = {showAllButtons}/> 
-        <ContactSection showAllButtons = {showAllButtons}/>
-        <AboutSection showAllButtons = {showAllButtons}/>
-        <EducationSection showAllButtons = {showAllButtons}/> 
-        <ExperienceSection showAllButtons = {showAllButtons}/> 
+        <IntroSection showAllButtons={showAllButtons} />
+        <ContactSection showAllButtons={showAllButtons} />
+        <AboutSection showAllButtons={showAllButtons} />
+        <EducationSection showAllButtons={showAllButtons} />
+        <ExperienceSection showAllButtons={showAllButtons} />
 
-        {showAllButtons &&  <button 
+        {showAllButtons && <button
           type='reset'
-          onClick = { this.hideButtons }
-          className = 'submit-cv-button' >Submit Form</button> } 
-      </section>  
+          onClick={this.hideButtons}
+          className='submit-cv-button'>Submit CV</button>}
+
+          {!showAllButtons && 
+          <section className='after-submit-buttons'>
+            <button
+              className='submit-cv-button'
+              onClick={this.hideButtons}>Resume</button> 
+            
+            <p className='bonus-info'>Reset the page for a new CV model!</p>
+            </section>
+            }
+      </section>
     )
   }
 };
